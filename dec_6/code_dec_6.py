a521485c720c63e2709c9fbd6d5b58768d42ed78
Teun <teunhanraets@gmail.com>
2022-12-06 10:40:01 +0100

    Improved formatting

--- dec_6/code_dec_6.py
+++ dec_6/code_dec_6.py
@@ -6,7 +6,6 @@ def find_distict_sequence(l):
         j = i + 4
         if len(set([x for x in line[j-l:j:]])) == l:
             return j
-            break
     
 print(f'Answer part 1: {find_distict_sequence(4)}')
 print(f'Answer part 2: {find_distict_sequence(14)}')
