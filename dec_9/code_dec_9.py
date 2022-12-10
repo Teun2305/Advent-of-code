dc6a80e1ab81b27007dee54608a6cc05ce67647c
Teun <teunhanraets@gmail.com>
2022-12-10 16:37:49 +0100

    Added solution dec 9 part 2

--- dec_9/code_dec_9.py
+++ dec_9/code_dec_9.py
@@ -19,15 +19,13 @@ class Rope:
             self.y += 1
         elif dir == 'D':
             self.y -= 1
-        else:
-            raise ValueError
         
         self.visited.append((self.x, self.y))
 
 
     def in_range(self):
-        return abs(self.x - self.parent.x) <= 1 and \
-            abs(self.y - self.parent.y) <= 1
+        return abs(self.parent.x - self.x) <= 1 and \
+            abs(self.parent.y - self.y) <= 1
 
 
     def follow(self):
@@ -37,21 +35,18 @@ class Rope:
             self.x += dif_x // 2
             self.y += dif_y // 2
             
-        elif (dif_x == 2 and dif_y == 1) or (dif_x == 1 and dif_y == 2):
+        elif (dif_x == 2 and dif_y == 1) or (dif_x == 1 and dif_y == 2) or (dif_x == 2 and dif_y == 2):
             self.x += 1
             self.y += 1
-        elif (dif_x == -2 and dif_y == 1) or (dif_x == -1 and dif_y == 2):
+        elif (dif_x == -2 and dif_y == 1) or (dif_x == -1 and dif_y == 2) or (dif_x == -2 and dif_y == 2):
             self.x -= 1
             self.y += 1
-        elif (dif_x == -2 and dif_y == -1) or (dif_x == -1 and dif_y == -2):
+        elif (dif_x == -2 and dif_y == -1) or (dif_x == -1 and dif_y == -2) or (dif_x == -2 and dif_y == -2):
             self.x -= 1
             self.y -= 1
-        elif (dif_x == 2 and dif_y == -1) or (dif_x == 1 and dif_y == -2):
+        elif (dif_x == 2 and dif_y == -1) or (dif_x == 1 and dif_y == -2) or (dif_x == 2 and dif_y == -2):
             self.x += 1
             self.y -= 1
-        else:
-            print_info()
-            raise ValueError
 
         self.visited.append((self.x, self.y))
 
@@ -59,31 +54,22 @@ class Rope:
     def get_number_locations(self):
         return len(set(self.visited))
 
-h, t = Rope(), Rope()
-t.set_parent(h)
 
+h = Rope()
 knots = [Rope() for _ in range(9)]
 knots[0].set_parent(h)
 for k1, k2 in zip(knots, knots[1::]):
     k2.set_parent(k1)
 
-def print_info():
-    print(f'h: {h.x} {h.y}')
-    for i, k in enumerate(knots):
-        print(f'{i}: {k.x} {k.y}')
-
 for line in f:
     dir, amount = line.split()
     amount = int(amount.strip('\n'))
-    print(dir, amount)
+
     for _ in range(amount): 
         h.move(dir)
-        '''if not t.in_range():
-            t.follow()'''
-        
-        for i, k in enumerate(knots):
+        for k in knots:
             if not k.in_range():
                 k.follow()
-
-print(f'Answer part 1: {t.get_number_locations()}')
+           
+print(f'Answer part 1: {knots[0].get_number_locations()}')
 print(f'Answer part 2: {knots[-1].get_number_locations()}')
